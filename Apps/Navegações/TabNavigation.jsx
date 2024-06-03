76eb1b3020b0cc056f8c3b85715a800fe04c5417
= <=>
2024-06-03 17:54:36 -0300

    GC

--- Apps/Navegações/TabNavigation.jsx
+++ Apps/Navegações/TabNavigation.jsx
@@ -5,18 +5,58 @@ import ExploreTelas from '../Screens/ExploreTelas';
 import AddTelaPostagem from '../Screens/AddTelaPostagem';
 import TelaPerfil from '../Screens/TelaPerfil';
 import TelaInicial from '../Screens/TelaInicial';
+import { FontAwesome } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
     <Tab.Navigator screenOptions={{
-        headerShown:false
+        headerShown:false,
+        tabBarActiveTintColor:'#06f91e'
     }}>
-        <Tab.Screen name= 'home' component={TelaInicial}/>
-        <Tab.Screen name= 'explore' component={ExploreTelas}/>
-        <Tab.Screen name= 'addpost' component={AddTelaPostagem}/>
-        <Tab.Screen name= 'profile' component={TelaPerfil}/>
+        <Tab.Screen name= 'home' component={TelaInicial}
+        options={{
+          tabBarLabel:({color})=>(
+            <Text style={{color:color,fontSize:12,marginBottom:3}}>Inicio</Text>
+      ),
+            tabBarIcon:({color,size})=>(
+              <FontAwesome name="home" size={size} color={color} />
+            )
+    }}
+        />
+        <Tab.Screen name= 'explore' component={ExploreTelas}
+        options={{
+          tabBarLabel:({color})=>(
+            <Text style={{color:color,fontSize:12,marginBottom:3}}>Pesquisar</Text>
+      ),
+            tabBarIcon:({color,size})=>(
+              <AntDesign name="search1" size={size} color={color} />
+            )
+    }}
+        />
+        <Tab.Screen name= 'addpost' component={AddTelaPostagem}
+        options={{
+          tabBarLabel:({color})=>(
+            <Text style={{color:color,fontSize:12,marginBottom:3}}>Adicionar Peça</Text>
+      ),
+            tabBarIcon:({color,size})=>(
+              <AntDesign name="upload" size={size} color={color} />
+            )
+    }}
+        />
+        <Tab.Screen name= 'profile' component={TelaPerfil}
+        options={{
+          tabBarLabel:({color})=>(
+            <Text style={{color:color,fontSize:12,marginBottom:3}}>Editar Pedidos</Text>
+      ),
+            tabBarIcon:({color,size})=>(
+              <AntDesign name="profile" size={size} color={color} />
+            )
+    }}
+        />
     </Tab.Navigator>
   )
 }
