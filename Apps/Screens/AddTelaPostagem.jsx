fc39e1ce860fdd88e8d1615b0d7510faac77f10b
= <=>
2024-06-05 17:20:09 -0300

    GC

--- Apps/Screens/AddTelaPostagem.jsx
+++ Apps/Screens/AddTelaPostagem.jsx
@@ -1,7 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import App from '../../App';
+import { getFirestore } from "firebase/firestore";
+
+
 
 export default function AddTelaPostagem() {
+
+  const db = getFirestore(App);
+  const getCategoryList=()=>{}
   return (
     <View>
       <Text>AddTelaPostagem</Text>
---
+++ firebaseconfig.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm_5ZLkIStZyvRLK3clsNWPVIDDL9mPaw",
+  authDomain: "gclab-d469b.firebaseapp.com",
+  projectId: "gclab-d469b",
+  storageBucket: "gclab-d469b.appspot.com",
+  messagingSenderId: "1049708867467",
+  appId: "1:1049708867467:web:c1c48a28b563c50842098e"
+};
+
+export const app = initializeApp(firebaseConfig);
